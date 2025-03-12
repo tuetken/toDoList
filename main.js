30f276ddcc92e19babeb39ef3af69376108373d0
Tyler <t.tuetken@outlook.com>
2025-03-12 17:18:51 -0400

    Updated JS variables to const instead of var/let... also created a line-thorugh function that removes/adds a line through a task depending on its current state, as well as a double click feature that fully removes an item from the list.

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 // assigns variable "addToDoButton" to HTML button "addToDo"
-let addToDoButton = document.getElementById("addToDo");
+const addToDoButton = document.getElementById("addToDo");
 // assigns variable "addToListButton" to HTML div "toDoContainer"
-let toDoContainer = document.getElementById("toDoContainer");
+const toDoContainer = document.getElementById("toDoContainer");
 // assigns variable "inputField" to HTML field "inputField"
-let inputField = document.getElementById("inputField");
+const inputField = document.getElementById("inputField");
 
 // adds an event listener for clicks onto the addToDoButton
 addToDoButton.addEventListener("click", function () {
   // creates a paragraph element in HTML for each click on the user's to do list
-  var paragraph = document.createElement("p");
+  const paragraph = document.createElement("p");
+  //
   paragraph.classList.add("paragraph-styling");
   // allows us to assign user input into the field and convert it to html (p element)
   paragraph.innerText = inputField.value;
   // assigns the location of paragraph element inside the toDoContainer div
   toDoContainer.appendChild(paragraph);
+  // resets input field after clicking button
+  inputField.value = "";
+  // puts line through item when clicking on it and removes when clicked again
+  paragraph.addEventListener("click", function () {
+    if (paragraph.style.textDecoration === "line-through") {
+      paragraph.style.textDecoration = "none";
+    } else {
+      paragraph.style.textDecoration = "line-through";
+    }
+  });
+  // removes from list if double-clicked, regardless of line-through status
+  paragraph.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(paragraph);
+  });
 });
