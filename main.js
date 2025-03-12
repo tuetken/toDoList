1ffbd230aa28601fcbc62b62a7e5590f6fd68acd
Tyler <t.tuetken@outlook.com>
2025-03-12 17:51:29 -0400

    Moved the paragraph variable outside the eventlistener function to allow for wider scope, and to add spacing between other event listeners. Also, created a function to alert the user when their field is blank to prevent empty task submissions.

--- main.js
+++ main.js
@@ -4,29 +4,45 @@ const addToDoButton = document.getElementById("addToDo");
 const toDoContainer = document.getElementById("toDoContainer");
 // assigns variable "inputField" to HTML field "inputField"
 const inputField = document.getElementById("inputField");
+const paragraph = document.createElement("p");
+
+// ensures theres not a blank submission for a task
+function isBlank() {
+  const userInput = inputField.value.trim();
+
+  if (userInput === "") {
+    alert("Empty Field");
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // adds an event listener for clicks onto the addToDoButton
 addToDoButton.addEventListener("click", function () {
+  if (isBlank()) {
+    return;
+  }
   // creates a paragraph element in HTML for each click on the user's to do list
-  const paragraph = document.createElement("p");
-  //
   paragraph.classList.add("paragraph-styling");
   // allows us to assign user input into the field and convert it to html (p element)
-  paragraph.innerText = inputField.value;
+  paragraph.innerText = inputField.value.trim();
   // assigns the location of paragraph element inside the toDoContainer div
   toDoContainer.appendChild(paragraph);
   // resets input field after clicking button
   inputField.value = "";
-  // puts line through item when clicking on it and removes when clicked again
-  paragraph.addEventListener("click", function () {
-    if (paragraph.style.textDecoration === "line-through") {
-      paragraph.style.textDecoration = "none";
-    } else {
-      paragraph.style.textDecoration = "line-through";
-    }
-  });
-  // removes from list if double-clicked, regardless of line-through status
-  paragraph.addEventListener("dblclick", function () {
-    toDoContainer.removeChild(paragraph);
-  });
+});
+
+// puts line through item when clicking on it and removes when clicked again
+paragraph.addEventListener("click", function () {
+  if (paragraph.style.textDecoration === "line-through") {
+    paragraph.style.textDecoration = "none";
+  } else {
+    paragraph.style.textDecoration = "line-through";
+  }
+});
+
+// removes from list if double-clicked, regardless of line-through status
+paragraph.addEventListener("dblclick", function () {
+  toDoContainer.removeChild(paragraph);
 });
