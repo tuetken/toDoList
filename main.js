a6100ba77cfaaf673bcfdc2b723bbf6954f1f0e7
Tyler <t.tuetken@outlook.com>
2025-03-13 12:59:27 -0400

    Cleaned up comments and removed unnecessary ones

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-// assigns variable "addToDoButton" to HTML button "addToDo"
-const addToDoButton = document.getElementById("addToDo");
-// assigns variable "addToListButton" to HTML div "toDoContainer"
-const toDoContainer = document.getElementById("toDoContainer");
-// assigns variable "inputField" to HTML field "inputField"
-const inputField = document.getElementById("inputField");
+const addToDoButton = document.getElementById("addToDo"); // + button
+const toDoContainer = document.getElementById("toDoContainer"); // holds tasks
+const inputField = document.getElementById("inputField"); // writes tasks
 
-// ensures theres not a blank submission for a task
+// ensures theres not a blank submission
 function isBlank() {
   const userInput = inputField.value.trim();
 
@@ -17,22 +14,21 @@ function isBlank() {
   }
 }
 
-// adds an event listener for clicks onto the addToDoButton
 addToDoButton.addEventListener("click", function () {
   if (isBlank()) {
     return;
   }
 
-  // creates a paragraph element in HTML for each click on the user's to do list
   const paragraph = document.createElement("p");
-  paragraph.classList.add("paragraph-styling");
-  // allows us to assign user input into the field and convert it to html (p element)
+
+  paragraph.classList.add("paragraph-styling"); // css styling
+
   paragraph.innerText = inputField.value.trim();
-  // assigns the location of paragraph element inside the toDoContainer div
+
   toDoContainer.appendChild(paragraph);
   saveTasks();
-  // resets input field after clicking button
-  inputField.value = "";
+
+  inputField.value = ""; // clears input for next task
 
   // puts line through item when clicking on it and removes when clicked again
   paragraph.addEventListener("click", function () {
